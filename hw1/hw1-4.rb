438f8c82c65cf3ce5b27a00fe9e109ed78eb67d0
Raphael Leu <raphleu@gmail.com>
2014-09-11 10:42:04 -0400

    got secant method working

--- hw1/hw1-4.rb
+++ hw1/hw1-4.rb
@@ -1,11 +1,9 @@
-#can ann push to git
-
 def bisection(absolute_tolerance, lower_bound, upper_bound, function)
 	if function == "fourthroottwo"
-		if (((lower_bound**4 - 2) * (upper_bound**4 - 2)) > 0)
+		if (send(function, lower_bound) * send(function, upper_bound)) > 0
 			puts "Root not bracketed!"
 		end
-		if (lower_bound**4 - 2 > 0)
+		if send(function, lower_bound) > 0
 			temp_lower_bound = lower_bound
 			lower_bound = upper_bound
 			upper_bound = temp_lower_bound
@@ -15,7 +13,7 @@ def bisection(absolute_tolerance, lower_bound, upper_bound, function)
 	 	number_of_iterations = 0
 	 	until new_y.abs < absolute_tolerance
 	 		dx /= 2
-	 		new_y = (lower_bound + dx)**4 - 2
+	 		new_y = send(function, lower_bound + dx)
 	 		if new_y < 0
 	 			lower_bound += dx
 	 		end
@@ -29,10 +27,27 @@ end
 
 
 def secant(absolute_tolerance, lower_bound, upper_bound, function)
-		puts "entered secant with function #{function} and absolute_tolerance #{absolute_tolerance}"
+	number_of_iterations = 0
+	old_x = lower_bound
+	new_x = upper_bound
+	old_y = send(function, old_x)
+	new_y = send(function, new_x)
+	until new_y.abs < absolute_tolerance
+		dx = (old_x - new_x) * new_y / (new_y - old_y)
+		old_x = new_x
+		old_y = new_y
+		new_x += dx
+		new_y = send(function, new_x)
+		number_of_iterations += 1
+	end
+	puts "Final y value: #{new_y}"
+	puts "Final x value: #{new_x}"
+	puts "Number of iterations: #{number_of_iterations}"
 end
 
-
+def fourthroottwo(x)
+	return x**4 - 2
+end
 #Here's where the stuff starts when you run the program
 
 functions = ["fourthroottwo"]
