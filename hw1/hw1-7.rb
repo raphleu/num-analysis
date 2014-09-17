4a8651124f14efbfa564bb85a0f9d37562d358ad
Raphael Leu <raphleu@gmail.com>
2014-09-17 14:24:32 -0400

    taylor series code

---
+++ hw1/hw1-7.rb
@@ -0,0 +1,18 @@
+def taylor_series(x_val)
+	actual_y = (Math::E)**(3.0 * x_val)
+	iterations = 0
+	calculated_y = 1.0
+	previous_increment = 1.0
+	while iterations < 100 and (calculated_y - actual_y).abs > 0.000000001
+		puts calculated_y
+		increment = previous_increment * 3.0 * x_val / (iterations + 1.0)
+		previous_increment = increment
+		calculated_y += increment
+		iterations += 1
+	end
+	puts "Iterations: #{iterations}, calculated value: #{calculated_y}, actual value: #{actual_y}"
+end
+
+puts "Please enter value for x: "
+x_val = gets.chomp.to_f
+taylor_series(x_val)
