e3f335baa0a914bae8372eea2deaec3715038b4b
Raphael Leu <raphleu@gmail.com>
2014-10-20 17:13:39 -0400

    latex formated stuff

---
+++ project2/latex_output_formatter.rb
@@ -0,0 +1,11 @@
+file_name = "input8_with_error.csv"
+output_file = open("latex_" + file_name, 'w')
+
+File.open(file_name) do |f|
+	f.each_line do |line|
+		row = line.chomp.split(",")
+		output_file.write("#{row[0]}&#{row[1]}&#{row[2][8..-1]}\\\\\n")
+	end
+end
+
+output_file.close
