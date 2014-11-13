f1fcd7559f24a0742bfb4057707311647520d140
Raphael Leu <raphleu@gmail.com>
2014-11-13 11:32:02 -0500

    median found

--- hw3/q4.rb
+++ hw3/q4.rb
@@ -18,12 +18,29 @@ def simpsons_rule(f, a, b, n)
 	return (h/3.0) * (send(f, a) + send(f, b) + k) 
 end
 
-def density_of_photons(l, h, c, k, t)
-	return ((8.0 * Math::PI) / (l**4)) * (1 / (Math::E**((h * c) / (l * k * t)) - 1))
+def density_of_photons(x)
+	return  (x**2 / ((Math::E**(x / (1 - x)) - 1) * (1 - x)**4) ) # * t**2
 end
 
 def simple(x)
 	return x ** 2
 end
 
-puts simpsons_rule("simple", 1.0, 5.0, 1000)
+#integral: 2.404113806318499
+#target value = 1.20205690315925
+#found upper bound: 0.70209382 (gets 1.20206)
+
+
+def photon_median()
+	target_value = 1.20205690315925
+	diff = 0
+	upper_bound = 0.7020926
+	while diff < target_value
+		diff = simpsons_rule("density_of_photons", 0.000001, upper_bound, 10000000)
+		upper_bound += 0.00000001
+	end
+	puts upper_bound - 0.00000001
+end	
+
+# puts simpsons_rule("density_of_photons", 0.000001, 0.99999, 100000)
+photon_median()
